5f228df619e31c3d7c618e4078e92a3bbc0c7581
Jose Da Silva <tjoses@minsait.com>
2024-05-22 05:16:50 -0300

    Primeiro projeto para treinamento em php

---
+++ blog/contato.php
@@ -0,0 +1,15 @@
+<?php
+    include_once("templates/header.php");
+    
+?>
+
+
+
+<h1>Contato</h1>    
+
+
+
+
+<?php
+    include_once("templates/footer.php");
+?>
---
+++ blog/templates/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <p>Hora de Coder &copy; 2024</p>
+</footer>
+</body>
+</html>
